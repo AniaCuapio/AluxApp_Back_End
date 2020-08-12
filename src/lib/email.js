ba72c1c64d04920204fee58eb21f489d9ea83cdd
Arianaomi <arianaomi.lp@gmail.com>
2020-08-11 19:13:51 -0500

    :zap: get token to reset password
    get token to reset password

---
+++ src/lib/email.js
@@ -0,0 +1,6 @@
+module.exports = {
+  user: '852935f24cbea6',
+  pass: 'accba9d95029b2',
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+}
--- src/usecases/users.js
+++ src/usecases/users.js
@@ -1,50 +1,90 @@
-const Users = require("../models/users");
-const bcrypt = require("../lib/bcrypt.js");
-const jwt = require("../lib/jwt");
+const Users = require('../models/users')
+const bcrypt = require('../lib/bcrypt.js')
+const jwt = require('../lib/jwt')
+const crypto = require('crypto')
+const sendEmail = require('../handlers/email')
 
 function getAll() {
-  return Users.find();
+  return Users.find()
 }
 function getById(userId) {
-  return Users.findById(userId);
+  return Users.findById(userId)
 }
 
 function create(userData) {
-  return Users.create(userData);
+  return Users.create(userData)
 }
 
 function remove(userId) {
-  return Users.findByIdAndDelete(userId);
+  return Users.findByIdAndDelete(userId)
 }
 
 function update(userID, newUserData) {
-  return Users.findByIdAndUpdate(userID, newUserData);
+  return Users.findByIdAndUpdate(userID, newUserData)
 }
 
 async function signUp(userData) {
-  const { password, email } = userData;
-  const userByEmail = await Users.findOne({ email });
+  const { password, email } = userData
+  const userByEmail = await Users.findOne({ email })
   if (userByEmail) {
-    throw new Error("email ya utilizado");
+    throw new Error('email ya utilizado')
   }
-  const encriptedPassword = await bcrypt.hash(password);
+  const encriptedPassword = await bcrypt.hash(password)
   return Users.create({
     ...userData,
     password: encriptedPassword,
-  });
+  })
 }
 
 async function login(email, password) {
-  const userByEmail = await Users.findOne({ email });
+  const userByEmail = await Users.findOne({ email })
   if (!userByEmail) {
-    throw new Error("Login error- Email");
+    throw new Error('Login error- Email')
   }
-  const isValid = await bcrypt.compare(password, userByEmail.password);
-  console.log(isValid, email, password, userByEmail);
+  const isValid = await bcrypt.compare(password, userByEmail.password)
+  console.log(isValid, email, password, userByEmail)
   if (!isValid) {
-    throw new Error("Login Error- Password");
+    throw new Error('Login Error- Password')
   }
-  return jwt.sign({ id: userByEmail._id });
+  return jwt.sign({ id: userByEmail._id })
+}
+
+//! Restablecer password
+
+async function generateTokenReset(email) {
+  const { tokenResetPassword, expiraResetPassword } = Users
+  console.log(tokenResetPassword)
+
+  const userByEmail = await Users.findOne({ email })
+  if (!userByEmail) {
+    throw new Error('Reset-password error- Email')
+  }
+  //* Generar token
+
+  userByEmail.tokenResetPassword = crypto.randomBytes(20).toString('hex')
+  userByEmail.expiraResetPassword = Date.now() + 3600000
+
+  await userByEmail.save()
+
+  return userByEmail.tokenResetPassword
+}
+
+// async function sendEmailPassword(email, resetUrl) {
+//   await sendEmail.sendEmail({
+//     email,
+//     subject: 'Password Reset',
+//     resetUrl,
+//     file,
+//   })
+// }
+
+async function resetPassword(tokenResetPassword) {
+  const userByToken = await Users.findOne({
+    tokenResetPassword,
+    expiraResetPassword: {
+      $gt: Date.now(),
+    },
+  })
 }
 
 module.exports = {
@@ -55,4 +95,6 @@ module.exports = {
   update,
   signUp,
   login,
-};
+  generateTokenReset,
+  resetPassword,
+}
